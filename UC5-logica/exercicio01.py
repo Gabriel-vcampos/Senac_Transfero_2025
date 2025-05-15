f9d59cd12aa64f4acd270af135a596d8790e44f7
Gabriel <sars041822@gmail.com>
2025-05-15 16:24:22 -0300

    commit

---
+++ UC5-logica/exercicio01.py
@@ -0,0 +1,11 @@
+a = int(input("Olá deseja um valor a seguir: "));
+b = int(input("Olá deseja um valor a seguir: "));
+
+if a > b:
+    print(a , "é maior que:" , b);
+elif a < b:
+    print(b , "é maior que:" , a);
+else:
+    print("Os dois números são iguais!")
+    
+
